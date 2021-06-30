cd99cd946830a57ad4db5deba30284981576455c
Maheshkoppisetti98 <68641728+Maheshkoppisetti98@users.noreply.github.com>
2021-06-30 15:54:53 +0530

    Add files via upload

---
+++ Language Basics/program3.java
@@ -0,0 +1,5 @@
+class sample{
+	public static void main(String[] args){
+		int c=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		System.out.print("The sum of "+args[0]+" and "+args[1]+" is "+c);
+}}
